367a8bbf654610ab9e94e1e576daabf32cfafbf7
complete0415jiyoung <complete0415@naver.com>
2025-04-20 17:52:43 +0900

    FEAT: 사진검색앱 첫화면 상태유지

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -8,22 +8,33 @@ import 'package:go_router/go_router.dart';
 final GoRouter router = GoRouter(
   initialLocation: Routes.searchImage,
   routes: [
-    ShellRoute(
-      builder: (context, state, child) => child,
-      routes: [
-        GoRoute(
-          path: Routes.searchImage,
-          builder: (context, state) => PhotoSearchScreen(viewModel: getIt()),
-        ),
-        GoRoute(
-          path: Routes.searchImageDetail, // '/image/:id'
-          name: 'imageDetail',
-          builder: (context, state) {
-            final id = int.parse(state.pathParameters['id']!);
-            return ImageDetailPage(id: id, viewModel: getIt());
-          },
+    StatefulShellRoute.indexedStack(
+      builder: (
+        context,
+        GoRouterState state,
+        StatefulNavigationShell navigationShell,
+      ) {
+        return navigationShell;
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.searchImage,
+              builder:
+                  (context, state) => PhotoSearchScreen(viewModel: getIt()),
+            ),
+          ],
         ),
       ],
+    ),
+    GoRoute(
+      path: Routes.searchImageDetail,
+      name: 'imageDetail',
+      builder: (context, state) {
+        final id = int.parse(state.pathParameters['id']!);
+        return ImageDetailPage(id: id, viewModel: getIt());
+      },
     ),
   ],
 );
